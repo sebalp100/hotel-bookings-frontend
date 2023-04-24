b496a360db3ac0e244de6ae1dc5a7b396ba52299
enoqueJonas <enoque.macanda@gmail.com>
2023-04-24 23:35:02 +0200

    ADD sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <div className="bg-neutral-900 w-60 p-3 flex flex-col text-white">
+    <div className="flex-1">top</div>
+    <div>bottom</div>
+  </div>
+);
+
+export default Sidebar;
