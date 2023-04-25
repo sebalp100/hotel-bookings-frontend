bed8fe09059ef2efd8bf1cd7d1042e296df414b4
SimonGideon <simongideon918@gmail.com>
2023-04-26 01:24:57 +0300

    Initial commit

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useGetRoomsQuery } from '../api/detail';
+import { useGetRoomDataQuery } from '../api/detail';
 import './css/home.css';
 
 function DetailRoom() {
   const { id } = useParams();
-  const { data, error, isLoading } = useGetRoomsQuery(id);
+  const { data, error, isLoading } = useGetRoomDataQuery(id);
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -21,6 +21,7 @@ function DetailRoom() {
     <div className="mainContainer">
       <span>
         {data.name}
+        
       </span>
     </div>
   );
