2f5bbdd2020c734a08bd0009c783c8aec70dc067
SimonGideon <simongideon918@gmail.com>
2023-04-26 01:06:33 +0300

    Remove icons

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
-import { useGetRoomDataQuery } from '../api/detail';
+import { useGetRoomsQuery } from '../api/detail';
+import './css/home.css';
 
-function RoomDetails() {
+function DetailRoom() {
   const { id } = useParams();
-  const { data, error, isLoading } = useGetRoomDataQuery(id);
-
+  const { data, error, isLoading } = useGetRoomsQuery(id);
   if (isLoading) {
     return <p>Loading...</p>;
   }
-
   if (error) {
     return (
       <p>
@@ -19,28 +17,13 @@ function RoomDetails() {
       </p>
     );
   }
-
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <img src={data.image_url} alt={data.name} />
-      <p>{data.description}</p>
-      <p>{data.tv}</p>
-      <p>{data.reserved}</p>
-      <p>{data.beds}</p>
-      <div className="icons">
-        <span>
-          <FaFacebookF />
-        </span>
-        <span>
-          <FaTwitter />
-        </span>
-        <span>
-          <FaYoutube />
-        </span>
-      </div>
+    <div className="mainContainer">
+      <span>
+        {data.name}
+      </span>
     </div>
   );
 }
 
-export default RoomDetails;
+export default DetailRoom;
