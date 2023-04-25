542d1f9ec66224b433b8d38d19d4a6240c7e1a72
enoqueJonas <enoque.macanda@gmail.com>
2023-04-26 00:24:06 +0200

    ADD sidebar skeleton

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MdHotel } from 'react-icons/md';
-import { DASHBOARD_SIDEBAR_LINKS } from '../lib/consts/navigation';
+import { Link } from 'react-router-dom';
+import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../lib/consts/navigation';
+import SidebarLink from './SidebarLink';
 
 const Sidebar = () => (
   <div className="bg-neutral-900 w-60 p-3 flex flex-col text-white">
@@ -8,14 +10,19 @@ const Sidebar = () => (
       <MdHotel fontSize={24} />
       <span className="text-neutral-100 text-lg">Hotel Booking</span>
     </div>
-    <div className="flex-1">
+    <div className="flex-1 flex flex-col">
       {DASHBOARD_SIDEBAR_LINKS.map((item) => (
-        <div key={item.key}>
-          {item.label}
-        </div>
+        <SidebarLink key={item.key} item={item} />
       ))}
     </div>
-    <div>bottom</div>
+    <div className="flex flex-row p-2">
+      {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
+        <Link to={item.path} key={item.key} className="flex-1">
+          <span>{item.icon}</span>
+        </Link>
+      ))}
+    </div>
+    <p className="p-2">@2023 Hotel Bookings</p>
   </div>
 );
 
