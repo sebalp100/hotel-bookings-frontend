33ea54b55e61289cbb04bd7ec6c1df1180672aae
enoqueJonas <enoque.macanda@gmail.com>
2023-04-27 20:02:34 +0200

    UODATE page Reservation

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import FormAddReservation from '../components/FormAddReservation';
+import Sidebar from '../components/Sidebar';
 
-const Reservation = () => {
-
-};
+const Reservation = () => (
+  <div className="mainContainer flex flex-row bg-neutral-100 h-screen width-screen overflow-hidden">
+    <Sidebar />
+    <FormAddReservation />
+  </div>
+);
 
 export default Reservation;
