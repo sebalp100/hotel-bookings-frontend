a8b514365f24e9c534a24a4a97f009946309372c
sebalp100 <sebalp100@gmail.com>
2023-05-02 15:01:28 -0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Login from './components/Login';
